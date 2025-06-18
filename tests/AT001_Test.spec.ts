375f6effb67f4085c29e2df89e4b135c6a4f4cc1
hina <hina.nazfatima@gmail.com>
2025-06-18 12:59:55 +1000

    updated changes as per PR

--- tests/AT001_Test.spec.ts
+++ tests/AT001_Test.spec.ts
@@ -5,5 +5,6 @@ test('Test has title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
+  //new line added to test git commit
 });
 
